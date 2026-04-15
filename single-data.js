4fe5cac17dc4ce812dbddf9f1fbf8da214958b75
patrick <patrickabboud01@gmail.com>
2026-04-15 22:25:52 +0200

    single menu added

---
+++ single-data.js
@@ -0,0 +1,38 @@
+const singleMenuData = [
+    { 
+        category: "Main Meals", 
+        name: "Falafel Burger", 
+        protein: 21, 
+        Kcal: 600,
+        price: 8.50,
+        image: "images/menu items/burger.png",
+        ingredients: "Bun, Falafel Patty, Tahini, Lettuce, Tomato"
+    },
+    { 
+        category: "Main Meals", 
+        name: "Lahm Baajin", 
+        protein: 26,
+        Kcal: 600, 
+        price: 7.00,
+        image: "images/menu items/lahm.png",
+        ingredients: "Flat Bread, Soy Chunks, Pomegranate Molasses, Onions"
+    },
+    { 
+        category: "Breakfast", 
+        name: "Oatmeal Bowl", 
+        protein: 18, 
+        Kcal: 450,
+        price: 5.50,
+        image: "images/menu items/oats.png",
+        ingredients: "Oats, Plant Milk, Dark Chocolate, Peanut Butter"
+    },
+    { 
+        category: "Snacks", 
+        name: "Lazy Cake", 
+        protein: 5, 
+        Kcal: 400,
+        price: 4.00,
+        image: "images/menu items/lazy cake.png",
+        ingredients: "Cacao, Biscuits, Coconut Oil, Vegan Milk"
+    }
+];
