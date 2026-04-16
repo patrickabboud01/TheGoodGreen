5989cf21e2193afd7323e18cef2da485ce659436
patrick <patrickabboud01@gmail.com>
2026-04-16 20:54:57 +0200

    karel fixes n ktr

--- js/data/menu-data.js
+++ js/data/menu-data.js
@@ -28,6 +28,17 @@ const rawMenuData = [
     },
     { 
         category: "breakfast", 
+        name: "Scrambled Egg Sandwich", 
+        protein: 25,
+        Kcal: 500, 
+        image: "images/menu items/EggSandwich.jpg",
+        fixed_ingredients: "Chickpea Flour ; Pain Demie ; Spices", 
+        removable_ingredients: "Tomato ; Lettuce", 
+        choice_title: "", choice_options: "", choice_type: "", choice_title_2: "", choice_options_2: "", choice_type_2: "" 
+    },
+
+    { 
+        category: "breakfast", 
         name: "Pancakes", 
         protein: 8, 
         Kcal : 400,
@@ -179,17 +190,7 @@ const rawMenuData = [
         removable_ingredients: "", 
         choice_title: "", choice_options: "", choice_type: "", choice_title_2: "", choice_options_2: "", choice_type_2: "" 
     },
-        { 
-        category: "dinner", 
-        name: "Scrambled Egg Sandwich", 
-        protein: 25,
-        Kcal: 500, 
-        image: "images/menu items/EggSandwich.jpg",
-        fixed_ingredients: "Chickpea Flour ; Pain Demie ; Spices", 
-        removable_ingredients: "Tomato ; Lettuce", 
-        choice_title: "", choice_options: "", choice_type: "", choice_title_2: "", choice_options_2: "", choice_type_2: "" 
-    },
-
+        
     { 
         category: "dinner", 
         name: "Pasta Salad & Grilled Potatoes", 
