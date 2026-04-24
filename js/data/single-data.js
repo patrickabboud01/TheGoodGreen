38533c9b5288588f6a77238602c0c78e25766ec7
patrick <patrickabboud01@gmail.com>
2026-04-24 11:59:08 +0200

    fixes

--- js/data/single-data.js
+++ js/data/single-data.js
@@ -1,23 +1,5 @@
 const singleMenuData = [
-    { 
-        category: "Main Meals", 
-        name: "Falafel Burger", 
-        protein: 21, 
-        Kcal: 600,
-        price: 8.50,
-        image: "images/menu items/burger.png",
-        ingredients: "Bun, Falafel Patty, Tahini, Lettuce, Tomato"
-    },
-    
-    { 
-        category: "Main Meals", 
-        name: "Lahm Baajin", 
-        protein: 26,
-        Kcal: 600, 
-        price: 7.00,
-        image: "images/menu items/lahm.png",
-        ingredients: "Flat Bread, Soy Chunks, Pomegranate Molasses, Onions"
-    },
+   
     { 
         category: "Breakfast", 
         name: "Oatmeal Bowl", 
@@ -25,15 +7,82 @@ const singleMenuData = [
         Kcal: 450,
         price: 5.50,
         image: "images/menu items/oats.png",
-        ingredients: "Oats, Plant Milk, Dark Chocolate, Peanut Butter"
+        ingredients: "Oats, Plant Milk, Dark Chocolate, Peanut Butter , Berries"
     },
     { 
         category: "Snacks", 
-        name: "Lazy Cake", 
+        name: "Lazy Cake 5pcs", 
         protein: 5, 
         Kcal: 400,
-        price: 4.00,
+        price: 6.00,
         image: "images/menu items/lazy cake.png",
         ingredients: "Cacao, Biscuits, Coconut Oil, Vegan Milk"
-    }
+    },
+    { 
+        category: "Snacks", 
+        name: "Lazy Cake for 12 prs", 
+        protein: 20, 
+        Kcal: 1600,
+        price: 24.00,
+        image: "images/menu items/lazy cake.png",
+        ingredients: "Cacao, Biscuits, Coconut Oil, Vegan Milk"
+    },
+     { 
+        category: "Main Meals", 
+        name: "Lahm Baajin 2pcs", 
+        protein: 26,
+        Kcal: 600, 
+        price: 7.00,
+        image: "images/menu items/lahm.png",
+        ingredients: "Flat Bread, Soy Chunks, Pomegranate Molasses, Onions"
+    },
+    { 
+        category: "Pastry", 
+        name: "Buns 6pcs", 
+        protein: 8, 
+        Kcal: 250,
+        price: 5.00,
+        image: "images/menu items/lazy cake.png",
+        ingredients: "Flour,Yeast,Salt,Olive Oil,Sugar,Water"
+    },
+    { 
+        category: "Pastry", 
+        name: "Olive Bread 4pcs", 
+        protein: 8, 
+        Kcal: 400,
+        price: 8.00,
+        image: "images/menu items/olivebuns.jpeg",
+        ingredients: "Flour,Yeast,Salt,Olive Oil,Olive Paste,Sugar,Water"
+    },
+     { 
+        category: "Breakfast", 
+        name: "Croissant Chocolate 2pcs", 
+        protein: 9, 
+        Kcal: 400 ,
+        image: "images/menu items/croissant1.png",
+       ingredients: "Croissant dough , Chocolate",
+       price: 5.00, 
+        image: "images/menu items/croissant1.png"
+    },
+     { 
+        category: "Breakfast", 
+        name: "Croissant Plain 2pcs", 
+        protein: 9, 
+        Kcal: 400 ,
+        image: "images/menu items/croissant1.png",
+        ingredients: "Croissant dough", 
+        price: 5.00, 
+       image: "images/menu items/croissant1.png"
+    },
+     { 
+        category: "Breakfast", 
+        name: "Croissant Zaatar 2pcs", 
+        protein: 9, 
+        Kcal: 400 ,
+        image: "images/menu items/croissant1.png",
+        ingredients: "Croissant dough , Chocolate", 
+        price: 5.00, 
+        image: "images/menu items/croissant1.png"
+    },
+   
 ];
